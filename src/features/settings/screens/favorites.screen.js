91004af434f153cf4aa17154e49f18d88445eb78
rezasadeghi3182h@gmail.com <rezasadeghi3182h@gmail.com>
2022-07-03 15:47:07 +0430

    add lottie with favorites screen

--- src/features/settings/screens/favorites.screen.js
+++ src/features/settings/screens/favorites.screen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
-import { Text, TouchableOpacity } from "react-native";
+import { TouchableOpacity, View } from "react-native";
 import styled from "styled-components/native";
+import Lottie from "lottie-react-native";
 import { FavoritesContext } from "../../../services/favorites/favorites.context";
 import { SafeArea } from "../../../components/safe-area/safe-area.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
@@ -10,6 +11,12 @@ import { RestaurantList } from "../../restaurant/components/restaurant-list.styl
 const FavoritesArea = styled(SafeArea)`
   align-items: center;
   justify-content: center;
+`;
+const AnimationWrapperFavorites = styled(View)`
+  position: absolute;
+  top: 20%;
+  width: 100%;
+  height: 500px;
 `;
 export const FavoritesScreen = ({ navigation }) => {
   const { favorites } = useContext(FavoritesContext);
@@ -31,7 +38,15 @@ export const FavoritesScreen = ({ navigation }) => {
     />
   ) : (
     <FavoritesArea>
-      <Text>No Favorites</Text>
+      <AnimationWrapperFavorites>
+        <Lottie
+          key="not-found"
+          source={require("../../../../assets/data-not-found.json")}
+          resizeMode="cover"
+          autoPlay
+          loop
+        />
+      </AnimationWrapperFavorites>
     </FavoritesArea>
   );
 };
